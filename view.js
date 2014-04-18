16185cbe3b2e96e49d81966f51a1f7ad982fa895
rocktronica <tommy@iamnotagoodartist.com>
2014-04-18 15:17:47 -0700

    keys module

--- view.js
+++ view.js
@@ -1,43 +1,3 @@
-var KEY_TO_INDEX = {
-    '90': 0,
-    '83': 1,
-    '88': 2,
-    '68': 3,
-    '67': 4,
-    '86': 5,
-    '71': 6,
-    '66': 7,
-    '72': 8,
-    '78': 9,
-    '74': 10,
-    '77': 11,
-    '188': 12,
-    '76': 13,
-    '190': 14,
-    '186': 15,
-
-    '81': 0 + 12,
-    '50': 1 + 12,
-    '87': 2 + 12,
-    '51': 3 + 12,
-    '69': 4 + 12,
-    '82': 5 + 12,
-    '53': 6 + 12,
-    '84': 7 + 12,
-    '54': 8 + 12,
-    '89': 9 + 12,
-    '55': 10 + 12,
-    '85': 11 + 12,
-    '73': 12 + 12,
-    '57': 13 + 12,
-    '79': 14 + 12,
-    '48': 15 + 12,
-    '80': 16 + 12,
-    '219': 17 + 12,
-    '187': 18 + 12,
-    '221': 19 + 12
-};
-
 var octave = 4;
 var waveType = Osci.defaultWaveType;
 
@@ -45,6 +5,7 @@ var polyphonic = true,
     oscis = [];
 
 var frequencies = new Frequencies();
+var keys = new Keys();
 
 var keydown = function(e) {
     if (e.repeat) { return; }
@@ -71,12 +32,12 @@ var keydown = function(e) {
 
     var oscisIndex = polyphonic ? e.which : 0;
 
-    if (KEY_TO_INDEX[e.which] !== undefined) {
+    if (keys.noteIndexAtKey(e.which) !== undefined) {
         oscis[oscisIndex] = oscis[oscisIndex] || new Osci({
             waveType: waveType
         });
         oscis[oscisIndex].playFrequency(
-            frequencies.at(octave * 12 + KEY_TO_INDEX[e.which])
+            frequencies.at(octave * 12 + keys.noteIndexAtKey(e.which))
         );
     }
 
